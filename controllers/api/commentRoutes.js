9c0022dc94e4830dd08b73a040169c4e9ceb8e55
Creature Feature <ivy.mccurdy@gmail.com>
2023-07-19 16:19:50 -0500

    began work on comment and thread routes, fixed character limit on blog post to accomadate more text

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,12 +4,12 @@ const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newPost = await Comment.create({
+    const newComment = await Comment.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newPost);
+    res.status(200).json(newComment);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -17,14 +17,14 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const postData = await Post.destroy({
+    const commentData = await Comment.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!postData) {
+    if (!commentData) {
       res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
--- controllers/api/threadRoutes.js
+++ controllers/api/threadRoutes.js
@@ -26,7 +26,7 @@ router.get("/:id", async (req, res) => {
     }
 
     res.status(200).json(threadData);
-    // render('homepage', products)
+
    } catch (err) {
     res.status(500).json(err);
   }
